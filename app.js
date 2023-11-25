2066024d8a9591b6e77b617e4d26aebcc243b528
Bruno Kiafuka <brunokiafuka@live.com.pt>
2023-11-25 23:04:13 +0000

    :sparkles: Custom router - SPA

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 import Store from './services/Store.js';
 import API from './services/API.js';
 import { loadData } from "./services/Menu.js";
+import Router from './services/Router.js';
+
 
 
 window.app = {
-    store: Store
+    store: Store,
+    router: Router
 }
 
 
 window.addEventListener("DOMContentLoaded", () => {
+    app.router.init();
     loadData();
 })
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/popstate_event
+window.addEventListener('popstate', event => {
+    Router.go(event.state.route, false);
+});
