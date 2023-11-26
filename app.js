e27b63e202972427b15294fc87b1bf7b2cc4634a
Bruno Kiafuka <brunokiafuka@live.com.pt>
2023-11-26 22:45:11 +0000

    :sparkles: Final bindings

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ import Router from './services/Router.js';
 
 // Link my Web Components
 import { MenuPage } from './components/MenuPage.js';
-
 import { OrderPage } from './components/OrderPage.js';
 import { DetailsPage } from './components/DetailsPage.js';
-/* import ProductItem from './components/ProductItem.js';
-import CartItem from './components/CartItem.js'; */
+import ProductItem from './components/ProductItem.js';
+import CartItem from './components/CartItem.js';
 
 window.app = {
     store: Store,
@@ -22,3 +21,10 @@ window.addEventListener("DOMContentLoaded", () => {
     loadData();
 })
 
+
+window.addEventListener("appcartchange", event => {
+    const badge = document.getElementById("badge");
+    const qty = app.store.cart.reduce((acc, item) => acc + item.quantity, 0);
+    badge.textContent = qty;
+    badge.hidden = qty == 0;
+});
